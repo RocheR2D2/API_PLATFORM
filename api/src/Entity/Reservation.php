d1cb1b59d2c11fb8bd84b1e5a6b102f76fe42938
rocher2d2 <rocher2d2@gmail.com>
2019-02-14 08:45:23 +0100

    droit d'accès des entites

--- api/src/Entity/Reservation.php
+++ api/src/Entity/Reservation.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\JoinColumn;
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\ReservationRepository")
@@ -59,6 +60,18 @@ class Reservation
     public function setReservationDate(\DateTimeInterface $reservation_date): self
     {
         $this->reservation_date = $reservation_date;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
