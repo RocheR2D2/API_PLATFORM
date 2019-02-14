4b6bccc346d0669baca42d954714afca2cc97cc9
rocher2d2 <rocher2d2@gmail.com>
2019-02-14 15:31:14 +0100

    ajouter datafixtrues

--- api/src/DataFixtures/CompanyFixtures.php
+++ api/src/DataFixtures/CompanyFixtures.php
@@ -9,7 +9,40 @@
 namespace App\DataFixtures;
 
 
-class CompanyFixtures
+use App\Entity\Company;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class CompanyFixtures extends Fixture
 {
 
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+        $dat = fopen(dirname(__FILE__).'/Data/airline_company.dat', 'r');
+        $faker = Faker\Factory::create('fr_FR');
+
+        while ($line = fgets($dat))
+        {
+            $linearray = explode(",",$line);
+
+            $company = new Company();
+            $company->setName(trim($linearray[1],"\""));
+            $company->setEmail($faker->email);
+            $company->setPhonenumber($faker->phoneNumber);
+            $manager->persist($company);
+
+        }
+
+
+        fclose($dat);
+
+        $manager->flush();
+    }
 }
--- api/src/DataFixtures/CrewFixtures.php
+++ api/src/DataFixtures/CrewFixtures.php
@@ -9,7 +9,32 @@
 namespace App\DataFixtures;
 
 
-class CrewFixtures
+use App\Entity\Crew;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class CrewFixtures extends Fixture
 {
 
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $crew = new Crew();
+            $crew->setFirstname($faker->firstName);
+            $crew->setLastname($faker->lastName);
+            $crew->setBirthday($faker->dateTimeThisCentury);
+            $crew->setGender($faker->boolean);
+            $manager->persist($crew);
+        }
+        $manager->flush();
+    }
 }
